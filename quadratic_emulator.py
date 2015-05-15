19e8869575a177d7dd4dc104da725fbfa9d78d8f
Simeon Bird <sbird@andrew.cmu.edu>
2015-05-15 16:17:55 -0400

    Move the rebinning into the emulator class, rather than the Simulation class. Abandon class hierarchy for Simulation in favour of just keeping FluxPowSimulation for simplicity

--- quadratic_emulator.py
+++ quadratic_emulator.py
@@ -35,42 +35,14 @@ def Hubble(zz, om, H0):
     #Conversion factor between s/km and h/Mpc is (1+z)/H(z)
     return 100*H0*math.sqrt(om*(1+zz)**3+(1-om))
 
-class Simulation(object):
-    """A class to store a single simulation snapshot and get the quantity from that simulation that needs to be interpolated.
-        All the loading functions should be created by overloading this class.
-        Should store parameter values."""
-    def __init__(self, filename, params):
-        self.filename = filename
-        self.params = params
-
-    def get_quantity(self):
-        """Function to get a quantity that we want to interpolate"""
-        raise NotImplementedError
-
-    def get_bins(self):
-        """Get the binned kvalues for this quantity"""
-        raise NotImplementedError
-
-    def get_param(self,paramname):
-        """Get the parameter values for this simulation snapshot, over which we will do the interpolation"""
-        return self.params[paramname]
-
-    def get_paramnames(self):
-        """Get possible parameter names"""
-        return self.params.keys()
-
-class FluxPowSimulation(Simulation):
-    """Specialist class for loading a flux power spectrum. This should handle all file loading, rebinning, etc."""
-    def __init__(self, filename, params, zz, om, H0=0.7, box=60., sdsskbins=np.array([])):
+class FluxPowSimulation(object):
+    """Class for loading and storing a flux power spectrum. This should handle all file loading, etc, but not rebinning"""
+    def __init__(self, filename, params, zz, om, H0=0.7, box=60.):
         self.H0 = H0
         self.box=box
-        #SDSS kbins, in s/km units.
-        if np.size(sdsskbins) == 0:
-            sdsskbins=np.array([0.00141,0.00178,0.00224,0.00282,0.00355,0.00447,0.00562,0.00708,0.00891,0.01122,0.01413,0.01778])
-        self.sdsskbins = sdsskbins
-        super(FluxPowSimulation, self).__init__(filename, params)
+        self.filename = filename
+        self.params = params
         (self.k, self.PF) = self.loadpk(zz, H0, om)
-        self.sdssPF = rebin(self.PF, self.k, self.sdsskbins)
 
     def loadpk(self, zz, H0, om):
         """Load a flux power spectrum in s/km units, from a text file."""
@@ -89,12 +61,21 @@ class FluxPowSimulation(Simulation):
         return (k, PF)
 
     def get_quantity(self):
-        """Return a flux power spectrum, rebinned to be in SDSS units"""
-        return self.sdssPF
+        """Return a flux power spectrum, in km/s units"""
+        return self.PF
 
     def get_bins(self):
-        """Get the binned kvalues for this quantity"""
-        return self.sdsskbins
+        """Get the binned kvalues in s/km"""
+        return self.k
+
+    def get_param(self,paramname):
+        """Get the parameter values for this simulation snapshot, over which we will do the interpolation"""
+        return self.params[paramname]
+
+    def get_paramnames(self):
+        """Get possible parameter names"""
+        return self.params.keys()
+
 
 class Knot(object):
     """A basic structure class to store a number of simulation runs, with parameter values."""
@@ -128,29 +109,35 @@ class QuadraticEmulator(object):
                      and then multiply to get the best fit.
                      The best fit used in each knot to compute the different coefficients need not be this simulation!
     """
-    def __init__(self, knots, bestfit):
+    def __init__(self, knots, bestfit, sdsskbins=np.array([])):
+        #SDSS kbins, in s/km units.
+        if np.size(sdsskbins) == 0:
+            sdsskbins=np.array([0.00141,0.00178,0.00224,0.00282,0.00355,0.00447,0.00562,0.00708,0.00891,0.01122,0.01413,0.01778])
+        self.sdsskbins = sdsskbins
         self.tables = {}
         #Compute coefficients separately for each parameter.
         for knot in knots:
-            self.tables[knot.name] = self.calc_coeffs(knot.bfnum, knot.sims,knot.name)
+            self.tables[knot.name] = self._calc_coeffs(knot.bfnum, knot.sims,knot.name)
         self.bestfitsim = bestfit
 
-    def get_quantity(self,newparams):
+    def get_interpolated(self,newparams):
         """Get the interpolated quantity by evaluating the quadratic fit"""
-        newq = self.bestfitsim.get_quantity()
+        #Interpolate onto desired bins
+        newq = rebin(self.bestfitsim.get_quantity(), self.bestfitsim.get_bins(), self.sdsskbins)
+        #Do parameter correction
         for (pp, nval) in newparams.iteritems():
             dp = nval-self.bestfitsim.get_param(pp)
             newq += self.tables[pp][:,0]*dp**2 +self.tables[pp][:,1]*dp
         return newq
 
-    def flux_deriv(self, PFdif, pdif):
+    def _flux_deriv(self, PFdif, pdif):
         """Calculate the flux-derivative for a single parameter change"""
         assert np.size(pdif) == np.size(PFdif)
         mat=np.vstack([pdif**2, pdif] ).T
         (derivs, _,_, _)=np.linalg.lstsq(mat, PFdif)
         return derivs
 
-    def calc_coeffs(self, bfnum, sims, paramname):
+    def _calc_coeffs(self, bfnum, sims, paramname):
         """
             Calculate the flux derivatives for a single redshift, for a single shifting parameter
             Input:
@@ -161,23 +148,36 @@ class QuadraticEmulator(object):
         #Get the quantity we want to interpolate: this will be an array of different scales.
         #So to_interp is (nsims, nbins)
         assert np.size(sims) > bfnum
-        to_interp = np.array([ss.get_quantity() for ss in sims])
+        to_interp = [ss.get_quantity() for ss in sims]
         kbins = sims[bfnum].get_bins()
         assert np.shape(to_interp) == (np.size(sims), np.size(kbins))
+        #Rebin the flux power for the k values we want
+        try:
+            bto_interp = np.array([rebin(tt, kbins, self.sdsskbins) for tt in to_interp])
+            #Compute differences in the interpolated quantity
+            dto_interp = bto_interp/bto_interp[bfnum,:] - 1.0
+        except ValueError:
+            #This will happen when we try to interpolate outside the allowed range.
+            #Happens with a box is smaller than 60 Mpc.
+            #Can also happen when the smallest kbin is too large, but won't handle that right now.
+            lowest = np.where(self.sdsskbins > kbins[0])[0][0]
+            #Set all changes outside the interpolation range to zero.
+            bto_interp = np.array([rebin(tt, kbins, self.sdsskbins[lowest:]) for tt in to_interp])
+            dto_interp = np.zeros((np.size(sims), np.size(self.sdsskbins)))
+            dto_interp[:,lowest:] = bto_interp/bto_interp[bfnum,:] - 1.0
+
         params = np.array([ss.get_param(paramname) for ss in sims])
         #Compute parameter differences
         params -= params[bfnum]
         #Make sure this parameter does change for the passed simulations
         assert np.any(params != 0)
-        #Compute differences in the interpolated quantity
-        dto_interp = to_interp/to_interp[bfnum,:] - 1.0
         #So now we have an array of data values.
         #Pass each k value to flux_deriv in turn.
         # Format of returned data from flux_derivs is (a,b) where it fits to:
         # dto_interp = a params**2 + b params
-        results =np.array([self.flux_deriv(dto_interp[:,k], params) for k in xrange(np.size(kbins))])
+        results =np.array([self._flux_deriv(dto_interp[:,k], params) for k in xrange(np.size(self.sdsskbins))])
         #So results should have shape
-        assert np.shape(results) == (np.size(kbins), 2)
+        assert np.shape(results) == (np.size(self.sdsskbins), 2)
         return results
 
 if __name__=='__main__':

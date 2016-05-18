2348ef7141020ca0afe9a7a70d3071782708d646
Simeon Bird <sbird4@jhu.edu>
2016-05-18 14:02:23 -0400

    Move plotting into own file

--- latin_hypercube.py
+++ latin_hypercube.py
@@ -159,22 +159,3 @@ def weight_cube(sample, means, sigmas):
     #This samples from the inverse CDF
     return norm(loc=means, scale=sigmas).ppf(sample)
 
-#Wrap the plotting scripts in a try block so it succeeds on X-less clusters
-try:
-    import matplotlib.pyplot as plt
-
-
-    def plot_points_hypercube(lhs_xval, lhs_yval, color="blue"):
-        """Make a plot of the hypercube output points positioned on a regular grid"""
-        ndivision = np.size(lhs_xval)
-        assert ndivision == np.size(lhs_yval)
-        xticks = np.linspace(0,1,ndivision+1)
-        plt.scatter(lhs_xval, lhs_yval, marker='o', s=300, color=color)
-        plt.grid(b=True, which='major')
-        plt.xticks(xticks)
-        plt.yticks(xticks)
-        plt.xlim(0,1)
-        plt.ylim(0,1)
-
-except ImportError:
-    pass
---
+++ plot_latin_hypercube.py
@@ -0,0 +1,15 @@
+"""Small script to plot latin hypercubes. Separate so it works without X forwarding"""
+import matplotlib.pyplot as plt
+import numpy as np
+import latin_hypercube
+def plot_points_hypercube(lhs_xval, lhs_yval, color="blue"):
+    """Make a plot of the hypercube output points positioned on a regular grid"""
+    ndivision = np.size(lhs_xval)
+    assert ndivision == np.size(lhs_yval)
+    xticks = np.linspace(0,1,ndivision+1)
+    plt.scatter(lhs_xval, lhs_yval, marker='o', s=300, color=color)
+    plt.grid(b=True, which='major')
+    plt.xticks(xticks)
+    plt.yticks(xticks)
+    plt.xlim(0,1)
+    plt.ylim(0,1)

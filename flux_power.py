8e75429a89eaaa63b8777f4af0c7e76eebe9a851
Simeon Bird <sbird4@jhu.edu>
2016-06-02 16:21:52 -0400

    Rework flux power so that it is clearer and constant spectral position is enforced

--- flux_power.py
+++ flux_power.py
@@ -1,52 +1,60 @@
 """Modules to generate the flux power spectrum from a simulation box."""
 from __future__ import print_function
+import os.path
 import numpy as np
 import spectra
 
 class MySpectra(object):
     """This class stores the randomly positioned sightlines once,
        so that they are the same for each emulator point."""
-    def __init__(self, numlos = 32000, box=60.):
+    def __init__(self, numlos = 32000):
         self.NumLos = numlos
-        self.axis = np.ones(self.NumLos)
-        #Sightlines at random positions
-        #Re-seed for repeatability
-        np.random.seed(23)
-        self.cofm = box*np.random.random_sample((self.NumLos,3))
         #Use the right values for SDSS or BOSS.
         self.spec_res = 200.
+        self.axis = np.ones(self.NumLos)
+        self.cofm = np.array([])
+        #Re-seed for repeatability
+        np.random.seed(23)
+        #Want output every 0.2 from z=4.2 to z=2.0
+        self.zout = np.arange(4.2,1.9,-0.2)
 
-    def _get_spectra_snap(self, snap, base, redshifts):
+    def _get_spectra_snap(self, snap, base, box=60.):
         """Get a snapshot with generated HI spectra"""
-        try:
-            #First try to reload a savefile
-            ss = spectra.Spectra(snap, base, self.cofm, self.axis, res=self.spec_res/4, savefile="lya_forest_spectra.hdf5",spec_res = self.spec_res, reload_file=False)
-        except IOError:
-            #If we couldn't, regenerate the spectra
-            ss = spectra.Spectra(snap, base, self.cofm, self.axis, res=self.spec_res/4, savefile="lya_forest_spectra.hdf5",spec_res = self.spec_res,reload_file=True)
-            #Now if the redshift is something we want, generate the flux power
-            if np.min(np.abs(ss.red - redshifts)) < 0.05:
-                ss.get_tau("H",1,1215)
+        #If savefile exists, reload. Otherwise do not.
+        reload_file = False
+        savefile = "lya_forest_spectra.hdf5"
+        if not os.path.exists(os.path.join(os.path.join(base,"snapdir_"+str(snap).rjust(3,'0')),savefile)):
+            reload_file = True
+            #Get some spectra positions if we don't already have them
+            if np.size(self.cofm) != 3*self.NumLos:
+                self.cofm = box*np.random.random_sample((self.NumLos,3))
+        #Get the snapshot
+        ss = spectra.Spectra(snap, base, self.cofm, self.axis, res=self.spec_res/4, savefile=savefile,spec_res = self.spec_res, reload_file=reload_file)
+        #Make sure we will use the same spectra positions for all future snapshots.
+        self.cofm = ss.cofm
+        self.axis = ss.axis
+        #Now if the redshift is something we want, generate the flux power
+        if np.min(np.abs(ss.red - self.zout)) < 0.05:
+            flux_power = ss.get_flux_power_1D("H",1,1215)
+            if reload_file:
                 ss.save_file()
-        return ss
+            return flux_power
+        return np.array([])
 
     def get_flux_power(self, base):
         """Get the flux power spectrum in the format used by McDonald 2004
         for a snapshot set."""
-        #Want output every 0.2 from z=4.2 to z=2.0
-        zout = np.arange(4.2,1.9,-0.2)
         fluxlist = []
         for snap in range(100):
-            try:
-                ss = self._get_spectra_snap(snap, base,zout)
-                #Now if the redshift is something we want, generate the flux power
-                if np.min(np.abs(ss.red - zout)) < 0.05:
-                    fluxlist.append(ss.get_flux_power_1D("H",1,1215))
-            except IOError:
+            if not os.path.exists(os.path.join(base,"snapdir_"+str(snap).rjust(3,'0'))):
                 #We ran out of snapshots
                 break
+            flux_power = self._get_spectra_snap(snap, base)
+            #Now if the redshift is something we want, generate the flux power
+            if np.size(flux_power) > 2:
+                fluxlist.append(flux_power)
         #Make sure we have enough outputs
-        assert len(fluxlist) == np.size(zout)
+        assert len(fluxlist) == np.size(self.zout)
         flux_power = np.array(fluxlist)
         return flux_power
 

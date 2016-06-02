ce3bc639e6824db596b8c652f6c400a2b2b8b1d2
Simeon Bird <sbird4@jhu.edu>
2016-06-01 22:14:48 -0400

    Rework emulator generation logic until we can load a flux power from it

--- coarse_grid.py
+++ coarse_grid.py
@@ -1,11 +1,13 @@
 """Generate a coarse grid for the emulator and test it"""
 from __future__ import print_function
 import os.path
+import json
 import numpy as np
 # import emcee
 
-# import gpemulator
+import gpemulator
 import latin_hypercube
+import flux_power
 from SimulationRunner import lyasimulation,clusters
 
 class Params(object):
@@ -23,6 +25,48 @@ class Params(object):
             name += nn+'%.1e' % pp
         return name
 
+    def dump(self,basedir, dumpfile="emulator_params.json"):
+        """Dump parameters to a textfile."""
+        with open(os.path.join(basedir, dumpfile), 'w') as jsout:
+            json.dump(self.__dict__, jsout)
+
+    def load(self,basedir, dumpfile="emulator_params.json"):
+        """Load parameters from a textfile."""
+        with open(os.path.join(basedir, dumpfile), 'w') as jsin:
+            self.__dict__ = json.load(jsin)
+
+    def get_dirs(self):
+        """Get the list of directories in this emulator."""
+        return self.sample_points.values()
+
+    def get_parameters(self):
+        """Get the list of parameter vectors in this emulator."""
+        #These are ordered, right?
+        assert self.get_dirs()[0] == self.sample_points[self.sample_points.keys()[0]]
+        return np.array(self.sample_points.keys())
+
+    def gen_simulations(self, nsamples,basedir, npart=256.,box=60,):
+        """Initialise the emulator by generating simulations for various parameters."""
+        LymanAlphaSim = clusters.hypatia_mpi_decorate(lyasimulation.LymanAlphaSim)
+        toeval = latin_hypercube.get_hypercube_samples(self.param_limits, nsamples)
+        self.sample_points = dict((ev, self.build_dirname(ev)) for ev in toeval)
+        self.dump(basedir)
+        #Generate ICs for each set of parameter inputs
+        for ev in toeval:
+            outdir = os.path.join(basedir, self.sample_points[ev])
+            assert self.param_names[0] == 'ns'
+            assert self.param_names[1] == 'As'
+            assert self.param_names[2] == 'heat_slope'
+            assert self.param_names[3] == 'heat_amp'
+            assert self.param_names[4] == 'hub'
+            #Use Planck 2015 cosmology
+            ss = LymanAlphaSim(outdir, box,npart, ns=ev[0], scalar_amp=ev[1],rescale_gamma=True, rescale_slope=ev[2], rescale_amp=ev[3], hubble=ev[4], omegac=0.25681, omegab=0.0483)
+            try:
+                ss.make_simulation()
+            except RuntimeError as e:
+                print(str(e), " while building: ",outdir)
+        return
+
 def lnlike_linear(params, *, gp=None, data=None):
     """A simple emcee likelihood function for the Lyman-alpha forest using the
        simple linear model with only cosmological parameters.
@@ -34,38 +78,17 @@ def lnlike_linear(params, *, gp=None, data=None):
     diff = predicted-data.pf
     return -np.dot(diff,np.dot(data.invcovar + np.identity(np.size(diff))/cov,diff))/2.0
 
-# def init_lnline(nsamples, data=None):
-#     """Initialise the emulator by loading the flux power spectra from the simulations."""
-#     #Parameter names
-#     params = Params()
-#     data = gpemulator.SDSSData()
+def init_lnlike(basedir, data=None):
+    """Initialise the emulator by loading the flux power spectra from the simulations."""
+    #Parameter names
+    params = Params()
+    params.load(basedir)
+    data = gpemulator.SDSSData()
     #Glob the directories? Or load from a text file?
     #Get unique values
-#     flux_vectors = np.array([get_flux_power(pp) for pp in params])
-#     gp = gpemulator.SkLearnGP(tau_means = params[:,0], ns = params[:,1], As = params[:,2], kf=data.kf, flux_vectors=flux_vectors)
-#     return gp, data
-
-def gen_simulations(nsamples,basedir, npart=256.,box=60,):
-    """Initialise the emulator by generating simulations for various parameters."""
-    params = Params()
-    LymanAlphaSim = clusters.hypatia_mpi_decorate(lyasimulation.LymanAlphaSim)
-    with open(os.path.join(basedir, "emulator_params.txt"),'w') as saved_params:
-        saved_params.write(str(params.param_names)+"\n")
-        toeval = latin_hypercube.get_hypercube_samples(params.param_limits, nsamples)
-        #Generate ICs for each set of parameter inputs
-        for ev in toeval:
-            outdir = os.path.join(basedir, params.build_dirname(ev))
-            saved_params.write(str(ev)+"  :  "+outdir+"\n")
-            assert params.param_names[0] == 'ns'
-            assert params.param_names[1] == 'As'
-            assert params.param_names[2] == 'heat_slope'
-            assert params.param_names[3] == 'heat_amp'
-            assert params.param_names[4] == 'hub'
-            #Use Planck 2015 cosmology
-            ss = lyasimulation.LymanAlphaSim(outdir, box,npart, ns=ev[0], scalar_amp=ev[1],rescale_gamma=True, rescale_slope=ev[2], rescale_amp=ev[3], hubble=ev[4], omegac=0.25681, omegab=0.0483)
-            try:
-                ss.make_simulation()
-            except RuntimeError as e:
-                print(str(e), " while building: ",outdir)
-    return
+    myspec = flux_power.MySpectra()
+    flux_vectors = np.array([myspec.get_flux_power(pp) for pp in params.get_dirs()])
+    pvals = params.get_parameters()
+    gp = gpemulator.SkLearnGP(tau_means = pvals[:,0], ns = pvals[:,1], As = pvals[:,2], kf=data.kf, flux_vectors=flux_vectors)
+    return gp, data
 

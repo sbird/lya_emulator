2ef26c599e7142add1aac55b7d33193aab600d9c
Simeon Bird <spb@ias.edu>
2016-07-15 14:23:08 -0400

    Implement self-shielding in the rate network

--- rate_network.py
+++ rate_network.py
@@ -6,14 +6,21 @@ import scipy.interpolate as interp
 class RateNetwork(object):
     """A rate network for neutral hydrogen following
     Katz, Weinberg & Hernquist 1996, eq. 28-32."""
-    def __init__(self,redshift, helium=0.24, converge = 1e-5):
+    def __init__(self,redshift, photo_factor = 1., helium=0.24, converge = 1e-5):
         self.recomb = RecombRates()
         self.photo = PhotoRates()
+        self.photo_factor = photo_factor
         self.helium = helium
         #proton mass in g
         self.protonmass=1.67262178e-24
         self.redshift = redshift
         self.converge = converge
+        zz = [0, 1, 2, 3, 4, 5, 6, 7,8]
+        #Tables for the self-shielding correction. Note these are not well-measured for z > 5!
+        gray_opac = [2.59e-18,2.37e-18,2.27e-18, 2.15e-18, 2.02e-18, 1.94e-18, 1.82e-18, 1.71e-18, 1.60e-18]
+        gamma_UVB = [3.99e-14, 3.03e-13, 6e-13, 5.53e-13, 4.31e-13, 3.52e-13, 2.678e-13,  1.81e-13, 9.43e-14]
+        self.Gamma_ss = interp.InterpolatedUnivariateSpline(zz, gamma_UVB)
+        self.Gray_ss = interp.InterpolatedUnivariateSpline(zz, gray_opac)
 
     def get_nH(self, density):
         """The hydrogen atom number density"""
@@ -23,7 +30,8 @@ class RateNetwork(object):
         """The neutral hydrogen number density. Eq. 33 of KWH."""
         alphaHp = self.recomb.alphaHp(temp)
         GammaeH0 = self.recomb.GammaeH0(temp)
-        return nh * alphaHp/ (alphaHp + GammaeH0 + self.photo.gH0(self.redshift)/ne)
+        photofac = self.photo_factor*self.self_shield_corr(nh, temp)
+        return nh * alphaHp/ (alphaHp + GammaeH0 + self.photo.gH0(self.redshift)/ne)*photofac
 
     def _nHp(self, nh, temp, ne):
         """The ionised hydrogen number density. Eq. 34 of KWH."""
@@ -34,19 +42,22 @@ class RateNetwork(object):
         yy = self.helium / 4 / (1 - self.helium)
         alphaHep = self.recomb.alphaHep(temp) + self.recomb.alphad(temp)
         alphaHepp = self.recomb.alphaHepp(temp)
-        GammaHe0 = self.recomb.GammaeHe0(temp) + self.photo.gHe0(self.redshift)/ne
-        GammaHep = self.recomb.GammaeHep(temp) + self.photo.gHep(self.redshift)/ne
+        photofac = self.photo_factor*self.self_shield_corr(nh, temp)
+        GammaHe0 = self.recomb.GammaeHe0(temp) + self.photo.gHe0(self.redshift)/ne*photofac
+        GammaHep = self.recomb.GammaeHep(temp) + self.photo.gHep(self.redshift)/ne*photofac
         return yy * nh / (1 + alphaHep / GammaHe0 + GammaHep/alphaHepp)
 
     def _nHe0(self, nh, temp, ne):
         """The neutral helium number density. Eq. 36 of KWH."""
         alphaHep = self.recomb.alphaHep(temp) + self.recomb.alphad(temp)
-        GammaHe0 = self.recomb.GammaeHe0(temp) + self.photo.gHe0(self.redshift)/ne
+        photofac = self.photo_factor*self.self_shield_corr(nh, temp)
+        GammaHe0 = self.recomb.GammaeHe0(temp) + self.photo.gHe0(self.redshift)/ne*photofac
         return self._nHep(nh, temp, ne) * alphaHep / GammaHe0
 
     def _nHepp(self, nh, temp, ne):
         """The doubly ionised helium number density. Eq. 37 of KWH."""
-        GammaHep = self.recomb.GammaeHep(temp) + self.photo.gHep(self.redshift)/ne
+        photofac = self.photo_factor*self.self_shield_corr(nh, temp)
+        GammaHep = self.recomb.GammaeHep(temp) + self.photo.gHep(self.redshift)/ne*photofac
         alphaHepp = self.recomb.alphaHepp(temp)
         return self._nHep(nh, temp, ne) * GammaHep / alphaHepp
 
@@ -70,6 +81,29 @@ class RateNetwork(object):
         ne = self.get_equilib_ne(density, temp)
         nh = self.get_nH(density)
         return self._nH0(nh, temp, ne) / nh
+
+    def self_shield_corr(self, nh, temp):
+        """Photoionisation rate as  a function of density from Rahmati 2012, eq. 14.
+        Calculates Gamma_{Phot} / Gamma_{UVB}.
+        Inputs: hydrogen density, temperature
+            n_H
+        The coefficients are their best-fit from appendix A."""
+        nSSh = 1.003*self.self_shield_dens(self.redshift, temp)
+        return 0.98*(1+(nh/nSSh)**1.64)**-2.28+0.02*(1+nh/nSSh)**-0.84
+
+    def self_shield_dens(self,redshift, temp):
+        """Calculate the critical self-shielding density. Rahmati 202 eq. 13.
+        gray_opac and gamma_UVB are parameters of the UVB used.
+        gray_opac is in cm^2 (2.49e-18 is HM01 at z=3)
+        gamma_UVB in 1/s (1.16e-12 is HM01 at z=3)
+        temp is particle temperature in K
+        f_bar is the baryon fraction. 0.17 is roughly 0.045/0.265
+        Returns density in atoms/cm^3"""
+        T4 = temp/1e4
+        G12 = self.Gamma_ss(redshift)/1e-12
+        return 6.73e-3 * (self.Gray_ss(redshift) / 2.49e-18)**(-2./3)*(T4)**0.17*(G12)**(2./3)
+        #Do not include the omega_b correction.
+        #*(self.f_bar/0.17)**(-1./3)
 
 class RecombRates(object):
     """Recombination rates and collisional ionization rates, as a function of temperature.
@@ -109,8 +143,7 @@ class RecombRates(object):
 class PhotoRates(object):
     """The photoionization rates for a given species.
     Eq. 29 of KWH 06. This is loaded from a TREECOOL table."""
-    def __init__(self, treecool_file="TREECOOL", factor=1.):
-        self.set_factor(factor)
+    def __init__(self, treecool_file="TREECOOL"):
         #Format of the treecool table:
         # log_10(1+z), Gamma_HI, Gamma_HeI, Gamma_HeII,  Qdot_HI, Qdot_HeI, Qdot_HeII,
         # where 'Gamma' is the photoionization rate and 'Qdot' is the photoheating rate.
@@ -123,21 +156,17 @@ class PhotoRates(object):
         self.Gamma_HeI = interp.InterpolatedUnivariateSpline(redshifts, photo_rates[:,1])
         self.Gamma_HeII = interp.InterpolatedUnivariateSpline(redshifts, photo_rates[:,2])
 
-    def set_factor(self, factor=1.):
-        """Set a photoionisation rate scaling factor."""
-        self.factor = factor
-
     def gHe0(self,redshift):
         """Get photo rate for neutral Helium"""
         log1z = np.log10(1+redshift)
-        return self.Gamma_HeI(log1z)*self.factor
+        return self.Gamma_HeI(log1z)
 
     def gHep(self,redshift):
         """Get photo rate for singly ionized Helium"""
         log1z = np.log10(1+redshift)
-        return self.Gamma_HeII(log1z)*self.factor
+        return self.Gamma_HeII(log1z)
 
     def gH0(self,redshift):
         """Get photo rate for neutral Hydrogen"""
         log1z = np.log10(1+redshift)
-        return self.Gamma_HI(log1z)*self.factor
+        return self.Gamma_HI(log1z)

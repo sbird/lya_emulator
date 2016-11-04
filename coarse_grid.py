dec90477858454683836fcd54016764bdb6e7662
Simeon Bird <sbird4@jhu.edu>
2016-11-04 13:33:15 -0400

    Rename sparse_param_limits back to param_limits to aoid breaking saved structures and also because it is a bit confusing

--- coarse_grid.py
+++ coarse_grid.py
@@ -15,15 +15,15 @@ import matplotlib.pyplot as plt
 
 class Params(object):
     """Small class to store parameter names and limits"""
-    def __init__(self, basedir, sparse_param_names=None, sparse_param_limits=None):
-        if sparse_param_names is None:
-            self.sparse_param_names = ['ns', 'As', 'heat_slope', 'heat_amp', 'hub']
+    def __init__(self, basedir, param_names=None, param_limits=None):
+        if param_names is None:
+            self.param_names = ['ns', 'As', 'heat_slope', 'heat_amp', 'hub']
         else:
-            self.sparse_param_names = sparse_param_names
-        if sparse_param_limits is None:
-            self.sparse_param_limits = np.array([[0.6, 1.5], [1.5e-9, 4.0e-9], [0., 0.5],[0.25,2],[0.65,0.75]])
+            self.param_names = param_names
+        if param_limits is None:
+            self.param_limits = np.array([[0.6, 1.5], [1.5e-9, 4.0e-9], [0., 0.5],[0.25,2],[0.65,0.75]])
         else:
-            self.sparse_param_limits = sparse_param_limits
+            self.param_limits = param_limits
         self.dense_param_name = ['tau0',]
         #Limits on factors to multiply the thermal history by.
         self.dense_param_limits = np.array([[0.3,3],])
@@ -34,27 +34,27 @@ class Params(object):
 
     def build_dirname(self,params):
         """Make a directory name for a given set of parameter values"""
-        assert len(params) == len(self.sparse_param_names)
+        assert len(params) == len(self.param_names)
         name = ""
-        for nn,pp in zip(self.sparse_param_names, params):
+        for nn,pp in zip(self.param_names, params):
             name += nn+'%.2g' % pp
         return name
 
     def dump(self, dumpfile="emulator_params.json"):
         """Dump parameters to a textfile."""
         #Arrays can't be serialised so convert them back and forth to lists
-        self.sparse_param_limits = self.sparse_param_limits.tolist()
+        self.param_limits = self.param_limits.tolist()
         self.sample_params = self.sample_params.tolist()
         with open(os.path.join(self.basedir, dumpfile), 'w') as jsout:
             json.dump(self.__dict__, jsout)
-        self.sparse_param_limits = np.array(self.sparse_param_limits)
+        self.param_limits = np.array(self.param_limits)
         self.sample_params = np.array(self.sample_params)
 
     def load(self,dumpfile="emulator_params.json"):
         """Load parameters from a textfile."""
         with open(os.path.join(self.basedir, dumpfile), 'r') as jsin:
             self.__dict__ = json.load(jsin)
-        self.sparse_param_limits = np.array(self.sparse_param_limits)
+        self.param_limits = np.array(self.param_limits)
         self.sample_params = np.array(self.sample_params)
 
     def get_dirs(self):
@@ -67,7 +67,7 @@ class Params(object):
 
     def build_params(self, nsamples):
         """Build a list of directories and parameters from a hyercube sample"""
-        self.sample_params = latin_hypercube.get_hypercube_samples(self.sparse_param_limits, nsamples)
+        self.sample_params = latin_hypercube.get_hypercube_samples(self.param_limits, nsamples)
         self.sample_dirs = [self.build_dirname(ev) for ev in self.sample_params]
 
     def gen_simulations(self, nsamples, npart=256.,box=60,):
@@ -78,11 +78,11 @@ class Params(object):
         #Generate ICs for each set of parameter inputs
         for ev,edir in zip(self.sample_params, self.sample_dirs):
             outdir = os.path.join(self.basedir, edir)
-            assert self.sparse_param_names[0] == 'ns'
-            assert self.sparse_param_names[1] == 'As'
-            assert self.sparse_param_names[2] == 'heat_slope'
-            assert self.sparse_param_names[3] == 'heat_amp'
-            assert self.sparse_param_names[4] == 'hub'
+            assert self.param_names[0] == 'ns'
+            assert self.param_names[1] == 'As'
+            assert self.param_names[2] == 'heat_slope'
+            assert self.param_names[3] == 'heat_amp'
+            assert self.param_names[4] == 'hub'
             #Use Planck 2015 cosmology
             ss = simulationics.SimulationICs(outdir=outdir, box=box,npart=npart, ns=ev[0], scalar_amp=ev[1], code_args={'rescale_gamma': True, 'rescale_slope': ev[2], 'rescale_amp' :ev[3]}, code_class=lyasimulation.LymanAlphaSim, hubble=ev[4], omegac=0.25681, omegab=0.0483)
             try:
@@ -104,9 +104,9 @@ class KnotParams(Params):
     """Specialise parameter class for an emulator using knots.
     Thermal parameters turned off."""
     def __init__(self, basedir):
-        sparse_param_names = ['AA', 'BB', 'CC', 'DD', 'hub']
-        sparse_param_limits = np.append(np.repeat(np.array([[0.6,1.5]]),4,axis=0),[[0.65,0.75]],axis=0)
-        Params.__init__(self, basedir, sparse_param_names = sparse_param_names, sparse_param_limits = sparse_param_limits)
+        param_names = ['AA', 'BB', 'CC', 'DD', 'hub']
+        param_limits = np.append(np.repeat(np.array([[0.6,1.5]]),4,axis=0),[[0.65,0.75]],axis=0)
+        Params.__init__(self, basedir, param_names = param_names, param_limits = param_limits)
 
     def gen_simulations(self, nsamples, npart=256.,box=60,):
         """Initialise the emulator by generating simulations for various parameters."""

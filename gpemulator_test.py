2acf36679bf33a285729e0338a4a3690009d46d0
Simeon Bird <sbird4@jhu.edu>
2016-11-07 16:58:11 -0500

    Add new test for the gp emulator

--- gpemulator_test.py
+++ gpemulator_test.py
@@ -25,3 +25,18 @@ def test_emu_single():
     gp = gpemulator.SkLearnGP(params=params, kf=kf, flux_vectors=params)
     predict,_ = gp.predict(np.array([0.5]).reshape(1,1))
     assert np.abs(predict - 0.5) < 0.01
+
+def test_emu_multi_param():
+    """Simplest model possible with multiple parameters.
+    One is linear multiplication, one is a squared term."""
+    kf = np.array([ 0.00141,  0.00178,  0.00224,  0.00282])
+    p1 = np.linspace(0.25,1.75,10)
+    p2 = np.linspace(0.1,1.,10)
+    p2 = np.tile(p2,10)
+    p1 = np.repeat(p1,10)
+    params = np.vstack([p1.T,p2.T]).T
+    flux_vectors = np.array([kf*100*(pp[0] + pp[1]**2) for pp in params])
+    print(np.shape(flux_vectors))
+    gp = gpemulator.SkLearnGP(params=params, kf=kf, flux_vectors=flux_vectors)
+    predict,_ = gp.predict(np.array([0.5,0.2]).reshape(1,-1))
+    assert np.sum(np.abs(predict - (0.5+0.2**2) * 100*kf)/predict) < 1e-4

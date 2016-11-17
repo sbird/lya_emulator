a2988d109d1880373197680269559877ddc15d63
Simeon Bird <sbird4@jhu.edu>
2016-11-17 15:49:27 -0500

    Add matter power test plot script

--- coarse_grid_plot.py
+++ coarse_grid_plot.py
@@ -5,17 +5,17 @@ import matplotlib
 import gpemulator
 import coarse_grid
 import flux_power
+import matter_power
 matplotlib.use('PDF')
 import matplotlib.pyplot as plt
 
-
 def plot_test_interpolate(emulatordir,testdir, mean_flux=True, max_z=4.2):
     """Make a plot showing the interpolation error."""
-    params = coarse_grid.Params(emulatordir)
+    params = coarse_grid.Emulator(emulatordir)
     params.load()
     data = gpemulator.SDSSData()
-    gp = params.get_emulator(data.get_kf(), mean_flux=mean_flux, max_z=max_z)
-    params_test = coarse_grid.Params(testdir)
+    gp = params.get_emulator(mean_flux=mean_flux, max_z=max_z)
+    params_test = coarse_grid.Emulator(testdir)
     params_test.load()
     myspec = flux_power.MySpectra(max_z=max_z)
     #Constant mean flux.
@@ -46,4 +46,28 @@ def plot_test_interpolate(emulatordir,testdir, mean_flux=True, max_z=4.2):
             plt.savefig(name+".pdf")
         print(name+".pdf")
         plt.clf()
+    return gp
+
+def plot_test_matter_interpolate(emulatordir,testdir, redshift=3.):
+    """Make a plot showing the interpolation error for the matter power spectrum."""
+    params = coarse_grid.MatterPowerEmulator(emulatordir)
+    params.load()
+    gp = params.get_emulator()
+    params_test = coarse_grid.MatterPowerEmulator(testdir)
+    params_test.load()
+    for pp in params_test.get_parameters():
+        dd = params_test.get_outdir(pp)
+        predicted,_ = gp.predict(pp)
+        exact = matter_power.get_matter_power(dd,params.kf, redshift=redshift)
+        ratio = predicted[0]/exact
+        name = params_test.build_dirname(pp)
+        plt.loglog(params.kf,ratio,label=name)
+    plt.xlabel(r"$k$ (h/kpc)")
+    plt.ylabel(r"Predicted/Exact")
+    plt.title("Matter power")
+    plt.legend(loc=0)
+    plt.show()
+    plt.savefig(testdir+"matter_power.pdf")
+    print(testdir+"matter_power.pdf")
+    plt.clf()
     return gp

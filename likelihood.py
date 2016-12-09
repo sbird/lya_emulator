a0b9903bd801b9c40fd891e9de462d117ca038c9
Simeon Bird <sbird4@jhu.edu>
2016-12-09 15:33:07 -0500

    Fixes to likelihood

--- likelihood.py
+++ likelihood.py
@@ -4,27 +4,28 @@ import numpy as np
 import emcee
 import coarse_grid
 import flux_power
+import gpemulator
 
 def lnlike_linear(params, gp=None, data=None):
     """A simple emcee likelihood function for the Lyman-alpha forest."""
     assert gp is not None
     assert data is not None
-    predicted,cov = gp.predict(params)
-    diff = predicted-data.pf
-    return -np.dot(diff,np.dot(data.invcovar + np.identity(np.size(diff))/cov,diff))/2.0
+    predicted,_ = gp.predict(params.reshape(1,-1))
+    diff = predicted[0]-data['pf']
+    return -np.dot(diff,np.dot(data['invcovar'],diff))/2.0
 
 def init_lnlike(basedir, datadir, mean_flux=False, max_z=4.2):
     """Initialise the emulator by loading the flux power spectra from the simulations."""
     #Parameter names
+    sdss = gpemulator.SDSSData()
+    data = {}
+    myspec = flux_power.MySpectra(max_z=max_z)
+    data['pf'] = myspec.get_flux_power(datadir,sdss.get_kf(),tau0_factors=[1.,])
+    #Use the SDSS covariance matrix
+    data['invcovar'] = sdss.get_icovar()
     params = coarse_grid.Emulator(basedir)
     params.load()
     gp = params.get_emulator(max_z=max_z, mean_flux=mean_flux)
-    myspec = flux_power.MySpectra(max_z=max_z)
-    data = {}
-    data['pf'] = myspec.get_flux_power(datadir,params.kf,tau0_factors=[1.,])
-    #Use the SDSS covariance matrix
-    sdss = gpemulator.SDSSData()
-    data['invcovar'] = sdss.get_icovar()
     return gp, data, params.get_param_limits(include_dense=mean_flux)
 
 def init_emcee(basedir, datadir, mean_flux=False):
@@ -42,4 +43,4 @@ def init_emcee(basedir, datadir, mean_flux=False):
     return sampler
 
 if __name__ == "__main__":
-    chains = init_emcee(os.path.expanduser("~/data/Lya_Boss/cosmo-only-emulator"), os.path.expanduser("~/data/Lya_Boss/cosmo-only-test/AA0.94BB1.2CC0.71DD1.2hub0.71"),mean_flux=True)
+    chains = init_emcee(os.path.expanduser("~/data/Lya_Boss/cosmo-only-emulator"), os.path.expanduser("~/data/Lya_Boss/cosmo-only-test/AA0.94BB1.2CC0.71DD1.2hub0.71/output/"),mean_flux=True)

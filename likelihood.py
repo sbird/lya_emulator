4358a93941960f5234a2e434f127e3488a6dbfbc
Simeon Bird <sbird4@jhu.edu>
2016-12-09 16:54:44 -0500

    Refactor likelihood code

--- likelihood.py
+++ likelihood.py
@@ -6,41 +6,52 @@ import coarse_grid
 import flux_power
 import gpemulator
 
-def lnlike_linear(params, gp=None, data=None):
-    """A simple emcee likelihood function for the Lyman-alpha forest."""
-    assert gp is not None
-    assert data is not None
-    predicted,_ = gp.predict(params.reshape(1,-1))
-    diff = predicted[0]-data['pf']
-    return -np.dot(diff,np.dot(data['invcovar'],diff))/2.0
+class LikelihoodClass(object):
+    """Class to contain likelihood computations."""
+    def __init__(self, basedir, datadir, mean_flux=True):
+        """Initialise the emulator by loading the flux power spectra from the simulations."""
+        #Parameter names
+        sdss = gpemulator.SDSSData()
+        myspec = flux_power.MySpectra(max_z=4.2)
+        self.data_fluxpower = myspec.get_flux_power(datadir,sdss.get_kf(),tau0_factors=[1.,])[0]
+        #Use the SDSS covariance matrix
+        self.data_icovar = sdss.get_icovar()
+        params = coarse_grid.Emulator(basedir)
+        params.load()
+        self.gpemu = params.get_emulator(max_z=4.2, mean_flux=mean_flux)
+        self.param_limits = params.get_param_limits(include_dense=mean_flux)
+        #Initialise sampler and make a few samples.
+        self.sampler = self.init_emcee()
 
-def init_lnlike(basedir, datadir, mean_flux=False, max_z=4.2):
-    """Initialise the emulator by loading the flux power spectra from the simulations."""
-    #Parameter names
-    sdss = gpemulator.SDSSData()
-    data = {}
-    myspec = flux_power.MySpectra(max_z=max_z)
-    data['pf'] = myspec.get_flux_power(datadir,sdss.get_kf(),tau0_factors=[1.,])
-    #Use the SDSS covariance matrix
-    data['invcovar'] = sdss.get_icovar()
-    params = coarse_grid.Emulator(basedir)
-    params.load()
-    gp = params.get_emulator(max_z=max_z, mean_flux=mean_flux)
-    return gp, data, params.get_param_limits(include_dense=mean_flux)
+    def lnlike_linear(self, params):
+        """A simple emcee likelihood function for the Lyman-alpha forest."""
+        #Set parameter limits as the hull of the original emulator.
+        if np.any(params < self.param_limits[:,0]) or np.any(params > self.param_limits[:,1]):
+            return -np.inf
+        predicted,_ = self.gpemu.predict(params.reshape(1,-1))
+        diff = predicted[0]-self.data_fluxpower
+        #TODO: emuerr should contain an estimate of the 'theory error' on the power spectrum.
+        emuerr = 1e99
+        return -np.dot(diff,np.dot(self.data_icovar+np.identity(np.size(diff))/emuerr,diff))/2.0
 
-def init_emcee(basedir, datadir, mean_flux=False):
-    """Initialise and run emcee."""
-    (gp, data, param_limits) = init_lnlike(basedir, datadir, mean_flux=mean_flux, max_z = 4.2)
-    #Number of knots plus one cosmology plus one for mean flux.
-    ndim = np.shape(param_limits)[0]
-    nwalkers = 100
-    #Limits: we need to hard-prior to the volume of our emulator.
-    pr = (param_limits[:,1]-param_limits[:,0])
-    pl = param_limits[:,0]
-    p0 = [pr*np.random.rand(ndim)+pl for i in range(nwalkers)]
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnlike_linear, args=[gp,data])
-    sampler.run_mcmc(p0, 1000)
-    return sampler
+    def init_emcee(self,nwalkers=100, burnin=1000, nsamples = 10000):
+        """Initialise and run emcee."""
+        #Number of knots plus one cosmology plus one for mean flux.
+        ndim = np.shape(self.param_limits)[0]
+        #Limits: we need to hard-prior to the volume of our emulator.
+        pr = (self.param_limits[:,1]-self.param_limits[:,0])
+        pl = self.param_limits[:,0]
+        p0 = [pr*np.random.rand(ndim)+pl for i in range(nwalkers)]
+        emcee_sampler = emcee.EnsembleSampler(nwalkers, ndim, self.lnlike_linear)
+        emcee_sampler.run_mcmc(p0, burnin)
+        emcee_sampler.reset()
+        emcee_sampler.run_mcmc(None, nsamples)
+        return emcee_sampler
+
+    def new_parameter_limits(self, sigma=3):
+        """Find a square region centered on the maximum
+        likelihood value of the parameters and covering all regions
+        with a likelihood value larger """
 
 if __name__ == "__main__":
-    chains = init_emcee(os.path.expanduser("~/data/Lya_Boss/cosmo-only-emulator"), os.path.expanduser("~/data/Lya_Boss/cosmo-only-test/AA0.94BB1.2CC0.71DD1.2hub0.71/output/"),mean_flux=True)
+    like = LikelihoodClass(os.path.expanduser("~/data/Lya_Boss/cosmo-only-emulator"), os.path.expanduser("~/data/Lya_Boss/cosmo-only-test/AA0.94BB1.2CC0.71DD1.2hub0.71/output/"),mean_flux=True)

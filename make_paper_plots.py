d24d02d002f2706233a93cb82ea2e54251e431c1
Simeon Bird <sbird4@jhu.edu>
2017-01-08 18:31:16 -0500

    Plot each parameter varying

--- make_paper_plots.py
+++ make_paper_plots.py
@@ -6,6 +6,8 @@ matplotlib.use("PDF")
 import matplotlib.pyplot as plt
 import latin_hypercube
 from plot_latin_hypercube import plot_points_hypercube
+import coarse_grid
+import gpemulator
 
 plotdir = path.expanduser("~/papers/emulator_paper_1/plots")
 def hypercube_plot():
@@ -25,6 +27,27 @@ def hypercube_plot():
     plt.savefig(path.join(plotdir,"latin_hypercube_good.pdf"))
     plt.clf()
 
+def single_parameter_plot():
+    """Plot change in each parameter of an emulator from direct simulations."""
+    emulatordir = path.expanduser("~/data/Lya_Boss/emulator_quadratic")
+    data = gpemulator.SDSSData()
+    kf = data.get_kf()
+    emu = coarse_grid.Emulator(emulatordir)
+    emu.load()
+    gp = emu.get_emulator(mean_flux=True, max_z=4.2)
+    params = emu.param_names
+    defpar = gp.params[5,:]
+    deffv = gp.flux_vectors[5,:]
+    for (name, index) in params.items():
+        ind = np.where((gp.params[:,index] != defpar[index])*(gp.params[:,-1]==defpar[-1]))
+        for i in np.ravel(ind):
+            tp = gp.params[i,index]
+            fp = (gp.flux_vectors[i,:]/deffv).reshape(-1,len(kf))
+            nred = np.shape(fp)[0]
+            plt.semilogx(kf, fp[7,:], label=name+"="+str(tp)+" (z=3)")
+        plt.legend()
+        plt.savefig(path.join(plotdir,"single_param_"+name+".pdf"))
+        plt.clf()
 
 if __name__ == "__main__":
     hypercube_plot()

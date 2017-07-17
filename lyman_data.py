f9147c4ee130ca88946176755e749fe7b19581d2
Simeon Bird <sbird4@jhu.edu>
2017-07-17 17:05:29 -0400

    Add BOSS data

--- lyman_data.py
+++ lyman_data.py
@@ -1,4 +1,5 @@
 """Module to load the covariance matrix (from BOSS DR9 or SDSS DR5 data) from tables."""
+import os.path
 import numpy as np
 
 class SDSSData(object):
@@ -38,3 +39,31 @@ class SDSSData(object):
     def get_covar(self):
         """Get the inverse covariance matrix"""
         return self.covar
+
+class BOSSData(SDSSData):
+    """A class to store the flux power and corresponding covariance matrix from BOSS."""
+    def __init__(self, datafile="data/boss_dr9_data/table4a.dat", covardir="data/boss_dr9_data"):
+        # Read SDSS best-fit data.
+        # Contains the redshift wavenumber from SDSS
+        # See Readme file.
+        # Fourth column: square roots of covariance diagonal
+        data = np.loadtxt(datafile)
+        self.redshifts = data[:,2]
+        self.kf = data[:,3]
+        self.pf = data[:,4]
+        self.covar_diag = np.sqrt(data[:,5]**2 + data[:,8]**2)
+        #The covariance matrix, correlating each k and z bin with every other.
+        #kbins vary first, so that we have 11 bins with z=2.2, then 11 with z=2.4,etc.
+        self.covar = np.zeros((len(self.redshifts),len(self.redshifts)))
+        for bb in range(12):
+            dfile = os.path.join(covardir,"cct4b"+str(bb+1)+".dat")
+            dd = np.loadtxt(dfile)
+            self.covar[35*bb:35*(bb+1),35*bb:35*(bb+1)] = dd
+
+    def get_covar(self, zbin=None):
+        """Get the covariance matrix"""
+        return self.covar * self.covar_diag
+
+    def get_covar_diag(self):
+        """Get the covariance matrix"""
+        return self.covar_diag

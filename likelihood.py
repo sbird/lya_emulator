Fix broadcasting bug which was making the emulator errors unlrealistically large

likelihood.py
@@ -112,9 +112,11 @@ class LikelihoodClass(object):
         sdssz = self.sdss.get_redshifts()
         for bb in range(nz):
             diff_bin = diff[nkf*bb:nkf*(bb+1)]
+            std_bin = std[0,nkf*bb:nkf*(bb+1)]
             covar_bin = self.sdss.get_covar(sdssz[bb])
+            assert np.shape(np.diag(std_bin**2)) == np.shape(covar_bin)
             if include_emu:
-                covar_bin += np.diag(std**2)
+                covar_bin += np.diag(std_bin**2)
             icov_bin = np.linalg.inv(covar_bin)
             chi2 += - np.dot(diff_bin, np.dot(icov_bin, diff_bin),)/2. - 0.5*np.log(np.linalg.det(covar_bin))
         assert 0 > chi2 > -2**31
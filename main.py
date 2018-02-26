a0dd164a0cdaac138ac9add0041046f02d7718af
Keir Kwame Rogers <kwame@copspc-kroge.fysik.su.se>
2018-02-26 15:00:28 +0100

    Modifications for testing

---
+++ main.py
@@ -0,0 +1,4 @@
+from make_paper_plots import *
+
+if __name__ == "__main__":
+    test_knot_plots(testdir='/share/hypatia/sbird/Lya_Boss/hires_knots_test', emudir='/share/hypatia/sbird/Lya_Boss/hires_knots')
--- make_paper_plots.py
+++ make_paper_plots.py
@@ -76,10 +76,12 @@ def test_s8_plots():
     quad_quad = coarse_grid_plot.plot_test_interpolate(quaddir, testdir,savedir=path.join(plotdir,"hires_s8_quad_quad"),emuclass=QuadraticEmulator)
     return (gp_emu, gp_quad, quad_quad)
 
-def test_knot_plots(mf=1):
+def test_knot_plots(mf=1, testdir = None, emudir = None):
     """Plot emulator test-cases"""
-    testdir = path.expanduser("~/data/Lya_Boss/hires_knots_test")
-    emudir = path.expanduser("~/data/Lya_Boss/hires_knots")
+    if testdir is None:
+        testdir = path.expanduser("~/data/Lya_Boss/hires_knots_test")
+    if emudir is None:
+        emudir = path.expanduser("~/data/Lya_Boss/hires_knots")
     gp_emu = coarse_grid_plot.plot_test_interpolate(emudir, testdir,savedir=path.join(plotdir,"hires_knots_mf"+str(mf)),mean_flux=mf)
     return gp_emu
 

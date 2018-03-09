a2fc6bf49f7b480637bb1e91496065a32f22325f
Keir Rogers <keir@hypatia-login.local>
2018-03-09 15:02:39 +0000

    Fixed data directory paths

--- make_paper_plots.py
+++ make_paper_plots.py
@@ -14,8 +14,8 @@ from plot_latin_hypercube import plot_points_hypercube
 import coarse_grid_plot
 
 #plotdir = path.expanduser("~/papers/emulator_paper_1/plots")
-#plotdir = '/home/keir/Plots/Emulator'
-plotdir = '/Users/kwame/Documents/emulator_paper_1/plots'
+plotdir = '/home/keir/Plots/Emulator'
+#plotdir = '/Users/kwame/Documents/emulator_paper_1/plots'
 def hypercube_plot():
     """Make a plot of some hypercubes"""
     limits = np.array([[0,1],[0,1]])

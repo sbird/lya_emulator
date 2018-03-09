ad3ce31158e621972a80fa7b433a951695f98f90
Keir <keir.rogers@fysik.su.se>
2018-03-09 17:03:59 +0000

    Change plot names

--- coarse_grid_plot.py
+++ coarse_grid_plot.py
@@ -35,7 +35,7 @@ def plot_convexhull(emulatordir):
                 ax.plot(projected[simplex, 0], projected[simplex, 1], 'k-')
     return hull
 
-def plot_test_interpolate(emulatordir,testdir, savedir=None, mean_flux=1, max_z=4.2, emuclass=None, kf_bin_nums=None):
+def plot_test_interpolate(emulatordir,testdir, savedir=None, plotname="", mean_flux=1, max_z=4.2, emuclass=None, kf_bin_nums=None):
     """Make a plot showing the interpolation error."""
     if savedir is None:
         savedir = emulatordir
@@ -76,7 +76,7 @@ def plot_test_interpolate(emulatordir,testdir, savedir=None, mean_flux=1, max_z=
         plt.plot(xx, np.exp(-xx**2/2)/np.sqrt(2*np.pi), ls="-", color="black")
         plt.plot(xx, np.exp(-xx**2/2/2**2)/np.sqrt(2*np.pi*2**2), ls="--", color="grey")
         plt.xlim(-6,6)
-        plt.savefig(os.path.join(savedir, "errhist_"+str(np.size(errlist))+".pdf"))
+        plt.savefig(os.path.join(savedir, "errhist_"+plotname+str(np.size(errlist))+".pdf"))
         plt.clf()
         #DONE
         nred = len(myspec.zout)
@@ -95,7 +95,7 @@ def plot_test_interpolate(emulatordir,testdir, savedir=None, mean_flux=1, max_z=
         plt.show()
         if mean_flux:
             name = name+"mf0.95"
-        name = re.sub(r"\.","_",str(name))+".pdf"
+        name = re.sub(r"\.","_",str(name))+plotname+".pdf"
         #So we can use it in a latex document
         plt.savefig(os.path.join(savedir, name))
         print(name)
@@ -107,7 +107,7 @@ def plot_test_interpolate(emulatordir,testdir, savedir=None, mean_flux=1, max_z=
         plt.plot(xx, np.exp(-xx**2/2)/np.sqrt(2*np.pi), ls="-", color="black")
         plt.plot(xx, np.exp(-xx**2/2/2**2)/np.sqrt(2*np.pi*2**2), ls="--", color="grey")
         plt.xlim(-6,6)
-        plt.savefig(os.path.join(savedir, "errhist.pdf"))
+        plt.savefig(os.path.join(savedir, "errhist"+plotname+".pdf"))
         plt.clf()
     return gp
 
--- main.py
+++ main.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     testdir = '/share/hypatia/sbird/Lya_Boss/hires_knots_test'
     emudir = '/share/hypatia/sbird/Lya_Boss/hires_knots'
 
-    test_knot_plots(testdir=testdir, emudir=emudir, kf_bin_nums=None)
+    test_knot_plots(testdir=testdir, emudir=emudir, plotname="All_kf", kf_bin_nums=None)
     #test_knot_plots(testdir='/Users/kwame/Simulations/Lya_Boss/hires_knots_test',emudir='/Users/kwame/Simulations/Lya_Boss/hires_knots')

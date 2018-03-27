5b59c21a45ff4e661504b5cfe5cd8671a935a31d
Keir Kwame Rogers <kwame@copspc-kroge.fysik.su.se>
2018-03-27 15:51:27 +0200

    Make plotting optional

--- plot_likelihood.py
+++ plot_likelihood.py
@@ -3,12 +3,14 @@ import numpy as np
 import matplotlib
 matplotlib.use('PDF')
 import matplotlib.pyplot as plt
-import corner
+#import corner
 
 from likelihood import *
 
 def make_plot(chainfile, savefile):
     """Make a plot of parameter posterior values"""
+    import corner
+
     with open(chainfile+"_names.txt") as ff:
         names = ff.read().split('\n')
     pnames = [i.split(' ')[0] for i in names if len(i) > 0]
@@ -17,10 +19,11 @@ def make_plot(chainfile, savefile):
     corner.corner(samples, labels=pnames)
     plt.savefig(savefile)
 
-def run_and_plot_likelihood_samples(testdir, emudir, savefile, chain_savedir=None, n_walkers=100, n_burn_in_steps=100, n_steps=400):
+def run_and_plot_likelihood_samples(testdir, emudir, savefile, plot=True, chain_savedir=None, n_walkers=100, n_burn_in_steps=100, n_steps=400):
     if chain_savedir is None:
         chain_savedir = testdir
     chainfile = chain_savedir + '/AA0.97BB1.3_chain.txt'
     like = LikelihoodClass(basedir=emudir, datadir=testdir+"/AA0.97BB1.3CC0.67DD1.3heat_slope0.083heat_amp0.92hub0.69/output")
     output = like.do_sampling(chainfile, nwalkers=n_walkers, burnin=n_burn_in_steps, nsamples=n_steps)
-    make_plot(chainfile, savefile)
+    if plot is True:
+        make_plot(chainfile, savefile)

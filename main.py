8afd7b0128a430e19cb708f5b1ec4c14d5eed1d4
Keir Kwame Rogers <kwame@copspc-kroge.fysik.su.se>
2018-03-27 10:48:49 +0200

    Compare emulator errors to measurement errors

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     emudir = sim_rootdir + '/Lya_Boss/hires_knots'
     #savedir = '/Users/kwame/Papers/emulator_paper_1/plots'
 
-    #test_knot_plots(testdir=testdir, emudir=emudir, plotname="_Two_kf2", kf_bin_nums=[0,1]) #[33,34])
-    plot_test_interpolate_kf_bin_loop(emudir, testdir, savedir=savedir, plotname="_Two_loop", kf_bin_nums=np.arange(2))
+    test_knot_plots(testdir=testdir, emudir=emudir, plotname="_All_kf", kf_bin_nums=None, data_err=True) #[33,34])
+    #plot_test_interpolate_kf_bin_loop(emudir, testdir, savedir=savedir, plotname="_Two_loop", kf_bin_nums=np.arange(2))

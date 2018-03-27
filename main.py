39eaa1a9c4d992ae034eb343c682072e5e0d2e76
Keir Kwame Rogers <kwame@copspc-kroge.fysik.su.se>
2018-03-27 15:21:28 +0200

    Make posterior plots

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 
 from make_paper_plots import *
 from coarse_grid_plot import *
+from plot_likelihood import *
 
 if __name__ == "__main__":
     sim_rootdir = sys.argv[1]
@@ -11,5 +12,7 @@ if __name__ == "__main__":
     emudir = sim_rootdir + '/Lya_Boss/hires_knots'
     #savedir = '/Users/kwame/Papers/emulator_paper_1/plots'
 
-    test_knot_plots(testdir=testdir, emudir=emudir, plotname="_All_kf", kf_bin_nums=None, data_err=True) #[33,34])
+    #test_knot_plots(testdir=testdir, emudir=emudir, plotname="_All_kf", kf_bin_nums=None, data_err=True) #[33,34])
     #plot_test_interpolate_kf_bin_loop(emudir, testdir, savedir=savedir, plotname="_Two_loop", kf_bin_nums=np.arange(2))
+
+    run_and_plot_likelihood_samples(testdir, emudir, savedir + '/likelihood_samples.pdf', n_burn_in_steps=5000, n_steps=5000)

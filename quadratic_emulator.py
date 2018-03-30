d62df8f0fb8d1d2c2a596b8f6da47c29360266ec
Simeon Bird <sbird@ucr.edu>
2018-03-29 19:10:29 -0700

    Further fix for quad emulator

--- quadratic_emulator.py
+++ quadratic_emulator.py
@@ -49,7 +49,7 @@ class QuadraticPoly(SkLearnGP):
             newq += self.tables[pp][:,0]*dp**2 +self.tables[pp][:,1]*dp
         mean = newq * self.bestfv
         std = 0.001*np.ones_like(mean)
-        return [mean,], std
+        return [mean,], [std,]
 
     def _flux_deriv(self, PFdif, pdif):
         """Calculate the flux-derivative for a single parameter change"""

9bc63ca7d49c44df7cbc37ad5b7f551c90f530ed
Keir Kwame Rogers <kwame@copspc-kroge.fysik.su.se>
2018-07-10 14:19:30 +0200

    Minor changes

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     plotname = sys.argv[3]
     chain_savedir = sys.argv[4]
 
-    testdir = sim_rootdir + '/hot_cold_test' #Lya_Boss/hires_s8_test' #/share/hypatia/sbird
-    emudir = sim_rootdir + '/hot_cold' #Lya_Boss/hires_s8'
+    testdir = sim_rootdir + '/hires_s8_test' #'/hot_cold_test' #/share/hypatia/sbird
+    emudir = sim_rootdir + '/hires_s8' #'/hot_cold'
 
     simulation_sub_directory1 = '/HeliumHeatAmp0.9/output'
     simulation_sub_directory2 = '/HeliumHeatAmp1.1/output'
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     #test_knot_plots(testdir=testdir, emudir=emudir, plotdir=savedir, plotname=plotname, mf=2, kf_bin_nums=None, data_err=False, max_z=4.2)
     #plot_test_interpolate_kf_bin_loop(emudir, testdir, savedir=savedir, plotname="_Two_loop", kf_bin_nums=np.arange(2))
 
-    output = run_and_plot_likelihood_samples(testdir, emudir, likelihood_samples_plot_savefile, plotname, plot=True, chain_savedir=chain_savedir, n_burn_in_steps=50, n_steps=150, while_loop=False, mean_flux_label='s', return_class_only=False, rescale_data_error=True, include_emulator_error=True) #, max_z=2.6)
+    output = run_and_plot_likelihood_samples(testdir, emudir, likelihood_samples_plot_savefile, plotname, plot=True, chain_savedir=chain_savedir, n_burn_in_steps=50, n_steps=150, while_loop=False, mean_flux_label='s', return_class_only=False, rescale_data_error=False, include_emulator_error=True) #, max_z=2.6)
     #make_plot(chain_savedir + '/AA0.97BB1.3_chain_20000_MeanFluxFactor.txt', likelihood_samples_plot_savefile)
     #output = make_plot_flux_power_spectra(testdir, emudir, flux_power_plot_savefile, mean_flux_label='c')
     #make_plot_compare_two_simulations(emudir, emudir, simulation_sub_directory1, simulation_sub_directory2, compare_plot_savefile)

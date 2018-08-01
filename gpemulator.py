532508fe600cad24c0dabfd4fd61772fdbb77f71
Simeon Bird <sbird@ucr.edu>
2018-08-01 16:01:08 -0700

    Restart the optimizer if it fails for some reason

--- gpemulator.py
+++ gpemulator.py
@@ -81,12 +81,12 @@ class SkLearnGP(object):
         noutput = np.shape(normspectra)[1]
         self.gp = GPy.models.GPRegression(params_cube, normspectra,kernel=kernel, noise_var=1e-10)
 
-        self.gp.optimize(messages=False) #True
-        print(self.gp)
+        status = self.gp.optimize(messages=True) #True
         #print('Gradients of model hyperparameters [after optimisation] =', self.gp.gradient)
         #Let's check that hyperparameter optimisation is converged
-        #self.gp.optimize_restarts(num_restarts=10)
-        #print(self.gp)
+        if status.status != 'Converged':
+            self.gp.optimize_restarts(num_restarts=10)
+        print(self.gp)
         #print('Gradients of model hyperparameters [after second optimisation (x 10)] =', self.gp.gradient)
 
     def _check_interp(self, flux_vectors):

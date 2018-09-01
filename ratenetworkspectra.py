023fecfa7877b61ebbf86faadf50d88ab9be7d50
Simeon Bird <sbird@ucr.edu>
2018-08-31 22:38:10 -0700

    Further fixes to the interpolation in the rate network

--- ratenetworkspectra.py
+++ ratenetworkspectra.py
@@ -13,7 +13,7 @@ def RateNetworkGasTest():
     randi = (np.max(gasprop.ienergygrid) - np.min(gasprop.ienergygrid)) * np.random.random(size=2000) + np.min(gasprop.ienergygrid)
     for dd, ii in zip(randd, randi):
         spl = gasprop.spline(dd, ii)
-        rate = np.log(gasprop.rates.get_neutral_fraction(np.exp(dd), ii))
+        rate = np.log(gasprop.rates.get_neutral_fraction(np.exp(dd), np.exp(ii)))
         assert np.abs(spl - rate) < 1e-5
 
 class RateNetworkGas(gas_properties.GasProperties):
@@ -24,28 +24,28 @@ class RateNetworkGas(gas_properties.GasProperties):
         self.temp_factor = 1
         self.gamma_factor = 1
         #Build interpolation
-        sz = 10**3
-        self.densgrid = np.log(np.logspace(-7, -2, 2*sz))
-        self.ienergygrid = np.linspace(20, 1000, sz)
+        sz = 500
+        self.densgrid = np.log(np.logspace(-7, np.log10(3), 2*sz))
+        self.ienergygrid = np.log(np.logspace(np.log10(20), np.log10(3e6), sz))
         dgrid, egrid = np.meshgrid(self.densgrid, self.ienergygrid)
-        self.lh0grid = np.log(self.rates.get_neutral_fraction(np.exp(dgrid), egrid))
+        self.lh0grid = np.log(self.rates.get_neutral_fraction(np.exp(dgrid), np.exp(egrid)))
         #We assume primordial helium
         self.spline = interp2d(self.densgrid, self.ienergygrid, self.lh0grid, kind='cubic')
 
     def get_reproc_HI(self, part_type, segment):
         """Get a neutral hydrogen fraction using a rate network which reads temperature and density of the gas."""
         #expecting units of atoms/cm^3
-        density = self.get_code_rhoH(part_type, segment)
+        density = np.log(self.get_code_rhoH(part_type, segment))
         #expecting units of 10^-10 ergs/g
-        ienergy = self.absnap.get_data(part_type, "InternalEnergy", segment=segment)*self.units.UnitInternalEnergy_in_cgs/1e10
-        outside = density > np.max(self.densgrid) + density < np.min(self.densgrid) + ienergy > np.max(self.ienergygrid) + ienergy < np.min(self.ienergygrid)
+        ienergy = np.log(self.absnap.get_data(part_type, "InternalEnergy", segment=segment)*self.units.UnitInternalEnergy_in_cgs/1e10)
+        outside = (density > np.max(self.densgrid)) + (density < np.min(self.densgrid)) + (ienergy > np.max(self.ienergygrid)) + (ienergy < np.min(self.ienergygrid))
         ii = np.where(np.logical_not(outside))
         nh0 = np.zeros_like(density)
-        nh0[ii] = np.exp(self.spline(np.log(density[ii]), ienergy[ii]))
+        nh0[ii] = np.exp(self.spline(density[ii], ienergy[ii]))
         ii = np.where(outside)
         if np.size(ii) > 0.05*np.size(nh0):
             print("Interpolation range misses %d of particles." % np.size(ii)/np.size(nh0))
-            nh0[ii] = np.array([self.rates.get_neutral_fraction(dd, ii) for (dd, ii) in zip(density[ii], ienergy[ii])])
+            nh0[ii] = np.array([self.rates.get_neutral_fraction(np.exp(dd), np.exp(ee)) for (dd, ee) in zip(density[ii], ienergy[ii])])
         return nh0
 
     def _get_ienergy_rescaled(self, density, ienergy, density0):

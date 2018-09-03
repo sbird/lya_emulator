Fix mpc units kf

coarse_grid.py
@@ -265,7 +265,9 @@ class Emulator(object):
             flux_vectors = np.array([ps.get_power_native_binning(mean_fluxes = mef) for mef in mean_fluxes for ps in powers])
             #'natively' binned k values in km/s units as a function of redshift
             kfkms = [ps.get_kf_kms() for mef in mean_fluxes for ps in powers]
-            kfmpc = ps.kf
+            #Same in all boxes
+            kfmpc = powers[0].kf
+            assert np.all(np.abs(powers[0].kf/ powers[-1].kf-1) < 1e-6)
             self.save_flux_vectors(aparams, kfmpc, kfkms, flux_vectors)
         assert np.shape(flux_vectors)[0] == np.shape(aparams)[0]
         if kfunits == "kms":

flux_power.py
@@ -73,7 +73,7 @@ class FluxPower(object):
                 mf = mean_fluxes[i]
             kf_sim, flux_power_sim = ss.get_flux_power_1D("H",1,1215, mean_flux_desired=mf)
             #Store k_F in comoving Mpc/h units, so that it is independent of redshift.
-            vscale = ss.velfac * 3.085678e24/ss.rscale
+            vscale = ss.velfac * 3.085678e24/ss.units.UnitLength_in_cm
             kf_sim *= vscale
             ii = np.where(kf_sim <= self.maxk)
             flux_arr = np.append(flux_arr,flux_power_sim[ii])
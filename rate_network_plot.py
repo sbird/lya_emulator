9b9f2158f4bea36aa2480d73f58037283775f80d
Simeon Bird <sbird@ucr.edu>
2018-09-12 18:40:12 -0700

    More plot tweaks

--- rate_network_plot.py
+++ rate_network_plot.py
@@ -5,22 +5,26 @@ matplotlib.use("PDF")
 import matplotlib.pyplot as plt
 from rate_network import RateNetwork
 
-def make_cont_plot(zfunc, rates, levels=None, title=""):
+def make_cont_plot(zfunc, rates, levels=None, title="", maxie=2000,maxd=-2):
     """Helper function to make a contour plot."""
-    dens = np.logspace(-6, -2, 100)
+    dens = np.logspace(-6., maxd, 100)
 
-    ienergy = np.linspace(100, 700, 100)
+    ienergy = np.linspace(50, maxie, 100)
 
     gridd, gridt = np.meshgrid(dens, ienergy)
     temp = rates.get_temp(np.median(gridd)*np.ones_like(gridt), gridt)
-    cs = plt.contour(gridd, temp/1e4, zfunc(gridd, gridt), levels=levels)
+    cs = plt.contour(gridd, temp/1e3, zfunc(gridd, gridt), levels=levels)
 
     plt.clabel(cs,inline=1, fmt="%g")
     plt.xscale('log')
+    plt.yscale('log')
 
+    tt = np.array([2000, 3000, 5000, 10000, 20000, 30000, 50000, 100000])
+    plt.yticks(tt//1000, [r"%d" % (t//1000) for t in tt])
     plt.xlabel(r"$\rho$ (cm$^{-3}$)")
-    plt.ylabel(r"T ($10^4$ K)")
+    plt.ylabel(r"T ($10^3$ K)")
     plt.title(title)
+    plt.ylim(3,100)
 
 def collisplot(zz=3):
     """Plot the effect of collisional ionization on the neutral fraction."""
@@ -43,7 +47,7 @@ def simplenh0frac(gridd, gridt, rates, helium=0.24):
     phfac = rates.recomb.alphaHp(temp) / rates.photo.gH0(rates.redshift) * (1 + 2 * yy)
     return nh * phfac
 
-def simpleplot(zz=3, collisional=True):
+def simpleplot(zz=3, collisional=True, maxie=2200, maxd=-3):
     """Plot the rate network neutral fraction against the simple rescaling formula."""
     rates = RateNetwork(zz,selfshield=False, recomb="C92",collisional=collisional)
 
@@ -53,7 +57,7 @@ def simpleplot(zz=3, collisional=True):
         nh0simple = simplenh0frac(gridd, gridt, rates)
         return nh0/nh0simple
 
-    make_cont_plot(ratio, rates, levels=np.array([0.8, 0.9, 0.95, 0.99, 1]), title =r"$n_{H0}$ (full) / $n_{H0}$ (simple formula)")
+    make_cont_plot(ratio, rates, levels=np.array([0.8, 0.9, 0.95, 0.99, 1]), title =r"$n_{H0}$ (full) / $n_{H0}$ (simple formula)", maxie=maxie, maxd=maxd)
 
 def neplot(zz=3):
     """Plot the rate network electron density against a simple estimate with fully ionized helium."""
@@ -68,10 +72,10 @@ def neplot(zz=3):
 
     make_cont_plot(ratio, rates, title ="ne Simple formula")
 
-def hfracplot(zz=3):
+def hfracplot(zz=3, maxie=2000, maxd=-2):
     """Plot the rate network electron density against a simple estimate with fully ionized helium."""
     rates = RateNetwork(zz,selfshield=False, recomb="C92")
-    make_cont_plot(rates.get_neutral_fraction, rates, title =r"$n_{H0}/n_{H+}$", levels=np.array([1e-5,1e-4,1e-3,1e-2]))
+    make_cont_plot(rates.get_neutral_fraction, rates, title =r"$n_{H0}/n_{H+}$", levels=np.array([1e-7, 1e-6, 1e-5,1e-4,1e-3,1e-2]), maxie=maxie, maxd=maxd)
 
 def hefracplot(zz=3, collisional=True):
     """Plot the rate network electron density against a simple estimate with fully ionized helium."""
@@ -89,5 +93,5 @@ def hefracplot(zz=3, collisional=True):
     make_cont_plot(ratio, rates, title =r"$n_{He+}/n_{He++}$", levels=np.array([0.001, 0.01, 0.1, 1, 10,100]))
 
 if __name__ == "__main__":
-    simpleplot(3)
+    simpleplot(2.4)
     plt.savefig("plots/neutral_fraction_simple.pdf")

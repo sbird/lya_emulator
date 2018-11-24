ec0f2ad1d62e703bf419a26ea436636e0bc08fcb
Simeon Bird <sbird@ucr.edu>
2018-11-23 19:50:13 -0800

    Fix savefile in chain making

--- make_chains.py
+++ make_chains.py
@@ -10,15 +10,20 @@ parser.add_argument('testdir', type=str, help='Directory to use for testing')
 args = parser.parse_args()
 
 testdata=os.path.join(args.testdir, "output")
-savefile = args.testdir +".txt"
+#Get a savefile
+root, testdir = os.path.split(args.testdir)
+if testdir == '':
+    testdir = os.path.split(root)
+savefile = testdir +".txt"
 if args.noemuerr:
     savefile += "-noemuerr"
 #Build the emulator
 if args.quadratic:
-    savefile = os.path.join("simulations/hires_s8_quadratic", args.testdir+".txt")
+    savefile = os.path.join("simulations/hires_s8_quadratic", savefile)
     like = LikelihoodClass(basedir=os.path.expanduser("simulations/hires_s8_quadratic"), emulator_class="quadratic")
 else:
-    savefile = os.path.join("simulations/hires_s8", args.testdir+".txt")
+    savefile = os.path.join("simulations/hires_s8", savefile)
     like = LikelihoodClass(basedir=os.path.join("simulations/hires_s8"))
 
+print("Save to", savefile)
 output = like.do_sampling(savefile, datadir=testdata, include_emulator_error = not args.noemuerr)

8afc79bb0921c2f0e75988158d7532050b875952
Simeon Bird <sbird@ucr.edu>
2018-11-27 14:03:10 -0800

    Small fix

--- make_chains.py
+++ make_chains.py
@@ -24,10 +24,10 @@ if args.noemuerr:
 #Build the emulator
 if args.quadratic:
     savefile = os.path.join("simulations/hires_s8_quadratic", savefile)
-    like = LikelihoodClass(basedir=os.path.expanduser("simulations/hires_s8_quadratic"), emulator_class="quadratic",t0_training_value=args.t0 )
+    like = LikelihoodClass(basedir=os.path.expanduser("simulations/hires_s8_quadratic"), emulator_class="quadratic",t0_training_value=args.tau0)
 else:
     savefile = os.path.join("simulations/hires_s8", savefile)
-    like = LikelihoodClass(basedir=os.path.join("simulations/hires_s8"), t0_training_value=args.t0)
+    like = LikelihoodClass(basedir=os.path.join("simulations/hires_s8"), t0_training_value=args.tau0)
 
 print("Save to", savefile)
 output = like.do_sampling(savefile, datadir=testdata, include_emulator_error = not args.noemuerr)

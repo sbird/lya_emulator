fd51de40fa6860489e9b2133030d9e35bcb91ce2
Simeon Bird <sbird@ucr.edu>
2018-12-11 22:40:33 -0800

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name="lyaemu",
+    version='1.0.0',
+    author="Simeon Bird and Keir Rogers",
+    author_email="spb@ias.edu",
+    url="http://github.com/sbird/lya_emulator",
+    description="Module for easily generating emulators for the lyman alpha forest from simulations",
+    packages = ['lyaemu', 'lyaemu.tests'],
+    requires=['numpy', 'fake_spectra','scipy', "GPy", "emcee", "h5py"],
+    package_data = {
+            'lyaemu': ['data/*',],
+           },
+    classifiers = ["Development Status :: 4 - Beta",
+                   "Intended Audience :: Developers",
+                   "Intended Audience :: Science/Research",
+                   "License :: OSI Approved :: MIT License",
+                   "Programming Language :: Python :: 3",
+                   "Topic :: Scientific/Engineering :: Astronomy",
+                   "Topic :: Scientific/Engineering :: Visualization"]
+)

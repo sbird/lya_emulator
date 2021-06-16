fix full covariance to return only relevant redshifts, fix covariance inversion to return matrix that runs from high to low redshift

lyaemu/likelihood.py
@@ -71,10 +71,13 @@ def invert_block_diagonal_covariance(full_covariance_matrix, n_blocks):
     inverse_covariance_matrix = np.zeros_like(full_covariance_matrix)
     nz = n_blocks
     nk = int(full_covariance_matrix.shape[0] / nz)
-    for z in range(nz): #Loop over blocks by redshift
+    for i, z in zip(range(nz), reversed(range(nz))): #Loop over blocks by redshift
         start_index = nk * z
         end_index = nk * (z + 1)
         inverse_covariance_block = npl.inv(full_covariance_matrix[start_index:end_index, start_index:end_index])
+        # Reverse the order of the full matrix so it runs from high to low redshift
+        start_index = nk * i
+        end_index = nk * (i + 1)
         inverse_covariance_matrix[start_index:end_index, start_index:end_index] = inverse_covariance_block
     return inverse_covariance_matrix
 
@@ -276,7 +279,8 @@ class LikelihoodClass:
         npt.assert_allclose(sdssz, self.zout, atol=1.e-16)
         #print('SDSS redshifts are', sdssz)
         if zbin < 0:
-            covar_bin = self.sdss.get_covar()
+            # Returns the covariance matrix in block format for all redshifts up to max_z (sorted low to high redshift)
+            covar_bin = self.sdss.get_covar()[:sdssz.shape[0]*self.kf.shape[0], :sdssz.shape[0]*self.kf.shape[0]]
         else:
             covar_bin = self.sdss.get_covar(sdssz[zbin])
         return covar_bin

lyaemu/lyman_data.py
@@ -117,7 +117,7 @@ class BOSSData(SDSSData):
         # Note, DR9 and DR14 datasets report correlation matrices,
         # hence the conversion factor (outer product of covar_diag)
         if zbin is None:
-            # return the full covariance matrix (all redshifts)
+            # return the full covariance matrix (all redshifts) sorted in blocks from low to high redshift
             return self.covar * np.outer(np.sqrt(self.covar_diag), np.sqrt(self.covar_diag))
         # return the covariance matrix for a specified redshift
         ii = np.where((self.redshifts < zbin + 0.01)*(self.redshifts > zbin - 0.01)) #Elements in full matrix for given z
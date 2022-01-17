e89700158f779dd1429a0ba0c2e0df8eb0ef2eab
jibancat <mho026@ucr.edu>
2022-01-16 22:57:17 -0800

    update the conversion function with test files

--- lyaemu/mf_emulator/dataloader.py
+++ lyaemu/mf_emulator/dataloader.py
@@ -36,14 +36,16 @@ def input_normalize(
 # put the folder name on top, easier to find and modify
 def folder_name(num1: int, res1: int, box1: int, num2: int, res2:int, box2: int, z: float):
     return "Lyaflux_{}_res{}box{}_{}_res{}box{}_{}".format(
-        num1, res1, box1, num2, res2, box2, str(z).replace(".", "_")
+        num1, res1, box1, num2, res2, box2, "{:.2g}".format(z).replace(".", "_")
     )
 
 def convert_h5_to_txt(
         lf_filename: str = "data/emu_smallbox_lowres/cc_emulator_flux_vectors_tau1000000.hdf5",
         hf_filename: str = "data/emu_smallbox_highres/cc_emulator_flux_vectors_tau1000000.hdf5",
+        test_filename: str = "data/emu_testset/cc_emulator_flux_vectors_tau1000000.hdf5",
         lf_json: str = "data/emu_smallbox_lowres/emulator_params.json",
         hf_json: str = "data/emu_smallbox_highres/emulator_params.json",
+        test_json: str = "data/emu_testset/emulator_params.json",
     ):
     """
     Convert the h5 files Martin gave me to txt files to be read by the dataloader.
@@ -57,28 +59,29 @@ def convert_h5_to_txt(
     zout : redshifts
 
     Note: save each redshift as separate folders
-    TODO: add testing simulations.
     """
 
     f_lf = h5py.File(lf_filename, "r")
     f_hf = h5py.File(hf_filename, "r")
-    # TODO: test files
+    f_test = h5py.File(test_filename, "r")
 
     with open(lf_json, "r") as param:
         param_lf = json.load(param)
     with open(hf_json, "r") as param:
         param_hf = json.load(param)
-    # TODO: test files
+    with open(test_json, "r") as param:
+        param_test = json.load(param)
 
     param_limits = np.array(param_lf["param_limits"])
     assert np.all(param_limits == np.array(param_hf["param_limits"]))
+    assert np.all(param_limits == np.array(param_test["param_limits"]))
 
     # make sure all keys are in the file
     keys = ['flux_vectors', 'kfkms', 'kfmpc', 'params', 'zout']
     for key in keys:
         assert key in f_lf.keys()
         assert key in f_hf.keys()
-        # TODO: test files
+        assert key in f_test.keys()
 
     print("Low-fidelity file:")
     print("----")
@@ -88,11 +91,12 @@ def convert_h5_to_txt(
     print("Shape of params", f_lf["params"].shape)
     print("Shape of kfkms", f_lf["kfkms"].shape)
     print("Shape of kfmpc", f_lf["kfmpc"].shape)
-    print("Shape of flux vectors\n", f_lf["flux_vectors"].shape)
+    print("Shape of flux vectors", f_lf["flux_vectors"].shape)
+    print("\n")
 
     # use kfmpc so all redshifts use the same k bins
     kfmpc = f_lf["kfmpc"][()]
-    assert np.all(kfmpc == f_hf["kfmpc"][()])
+    assert np.all(np.abs(kfmpc - f_hf["kfmpc"][()]) < 1e-10)
 
     zout = f_lf["zout"][()]
     assert np.all(zout == f_hf["zout"][()])
@@ -126,7 +130,8 @@ def convert_h5_to_txt(
     print("Shape of params", f_hf["params"].shape)
     print("Shape of kfkms", f_hf["kfkms"].shape)
     print("Shape of kfmpc", f_hf["kfmpc"].shape)
-    print("Shape of flux vectors\n", f_hf["flux_vectors"].shape)
+    print("Shape of flux vectors", f_hf["flux_vectors"].shape)
+    print("\n")
 
     # flux power spectra, all redshifts
     flux_vectors_hf = f_hf["flux_vectors"][()]
@@ -136,21 +141,47 @@ def convert_h5_to_txt(
 
     # some checking
     last_flux_vector = get_flux_vector_at_z(len(zout) - 1, flux_vectors_hf)
-    assert len(last_flux_vector) == f_lf["params"].shape[0]
+    assert len(last_flux_vector) == f_hf["params"].shape[0]
     assert last_flux_vector.shape[1] == len(kfmpc)
 
     first_flux_vector = get_flux_vector_at_z(0, flux_vectors_hf)
-    assert len(first_flux_vector) == f_lf["params"].shape[0]
+    assert len(first_flux_vector) == f_hf["params"].shape[0]
     assert first_flux_vector.shape[1] == len(kfmpc)
 
-    # TODO: test files
+    # test files: same resolution as high-fidelity
+    print("Test file:")
+    print("----")
+    print("Resolution:", param_test["npart"])
+    print("Box (Mpc/h):", param_test["box"])
+    print("Shape of redshfits", f_test["zout"].shape)
+    print("Shape of params", f_test["params"].shape)
+    print("Shape of kfkms", f_test["kfkms"].shape)
+    print("Shape of kfmpc", f_test["kfmpc"].shape)
+    print("Shape of flux vectors", f_test["flux_vectors"].shape)
+    print("\n")
+
+    # flux power spectra, all redshifts
+    flux_vectors_test = f_test["flux_vectors"][()]
+
+    # input parameters
+    x_train_test = f_test["params"][()]
+
+    # some checking
+    last_flux_vector = get_flux_vector_at_z(len(zout) - 1, flux_vectors_test)
+    assert len(last_flux_vector) == f_test["params"].shape[0]
+    assert last_flux_vector.shape[1] == len(kfmpc)
+
+    first_flux_vector = get_flux_vector_at_z(0, flux_vectors_test)
+    assert len(first_flux_vector) == f_test["params"].shape[0]
+    assert first_flux_vector.shape[1] == len(kfmpc)
 
     # output training files, one redshift per folder
     for i,z in enumerate(zout):
         print("Preparing training files in {:.3g}".format(z))
 
-        flux_vector_lf = get_flux_vector_at_z(i, flux_vector_lf)
-        flux_vector_hf = get_flux_vector_at_z(i, flux_vector_hf)
+        flux_vector_lf = get_flux_vector_at_z(i, flux_vectors_lf)
+        flux_vector_hf = get_flux_vector_at_z(i, flux_vectors_hf)
+        flux_vector_test = get_flux_vector_at_z(i, flux_vectors_test)
 
         outdir = folder_name(
             len(x_train_lf),
@@ -161,25 +192,28 @@ def convert_h5_to_txt(
             param_lf["box"],
             z,
         )
-        os.makedirs(
-            os.path.join(
+
+        this_outdir = os.path.join(
                 "data",
+                "processed",
                 outdir,
-                exist_ok=True,
-            )
+        )
+        os.makedirs(
+            this_outdir,
+            exist_ok=True,
         )
 
-
         # only flux power needs a loop
-        np.savetxt(os.path.join("data", outdir, "train_output_fidelity_0.txt"), flux_vector_lf)
-        np.savetxt(os.path.join("data", outdir, "train_output_fidelity_1.txt"), flux_vector_hf)
+        np.savetxt(os.path.join(this_outdir, "train_output_fidelity_0.txt"), flux_vector_lf)
+        np.savetxt(os.path.join(this_outdir, "train_output_fidelity_1.txt"), flux_vector_hf)
+        np.savetxt(os.path.join(this_outdir, "test_output.txt"), flux_vector_test)
 
-        np.savetxt(os.path.join("data", outdir, "train_input_fidelity_0.txt"), x_train_lf)
-        np.savetxt(os.path.join("data", outdir, "train_input_fidelity_1.txt"), x_train_hf)
+        np.savetxt(os.path.join(this_outdir, "train_input_fidelity_0.txt"), x_train_lf)
+        np.savetxt(os.path.join(this_outdir, "train_input_fidelity_1.txt"), x_train_hf)
+        np.savetxt(os.path.join(this_outdir, "test_input.txt"), x_train_test)
 
-        np.savetxt(os.path.join("data", outdir, "input_limits.txt"), param_limits)
-        np.savetxt(os.path.join("data", outdir, "kf.txt"), kfmpc)
-        # TODO: test files
+        np.savetxt(os.path.join(this_outdir, "input_limits.txt"), param_limits)
+        np.savetxt(os.path.join(this_outdir, "kf.txt"), kfmpc)
 
 
 class FluxVectors:

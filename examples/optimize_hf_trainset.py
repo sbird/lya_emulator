6995466a71fd0c3ba88c041ab5af2b64899c01b9
jibancat <mho026@ucr.edu>
2022-02-16 21:07:09 -0800

    add an assert

--- examples/optimize_hf_trainset.py
+++ examples/optimize_hf_trainset.py
@@ -21,6 +21,9 @@ def hf_optimize(
 
     flux_lf = FluxVectorLowFidelity(filename, json_name)
 
+    # your previously selected index is shorted than num_selected
+    assert len(selected_index) + 1 == num_selected
+
     # if you previously selected index, direct search the num_selected - 1, and
     # search the (num_selected)th one.
     if selected_index is None:

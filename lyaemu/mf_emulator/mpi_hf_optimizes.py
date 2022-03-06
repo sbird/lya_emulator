0f416dc609114b350cebcfb93699bbb2662fb572
jibanCat <r04244007@ntu.edu.tw>
2022-03-05 20:03:23 -0800

    make MPI outside of the loop of redshifts

--- lyaemu/mf_emulator/mpi_hf_optimizes.py
+++ lyaemu/mf_emulator/mpi_hf_optimizes.py
@@ -26,30 +26,32 @@ def direct_search(
         loss = sum_{zi}^{zf} (MSE(z))
     """
 
-
     # looking for all possible combinations
     all_combinations = list(combinations(range(data.num_samples), num_selected))
 
+    # MPI World:
+    # ----
+    comm = MPI.COMM_WORLD     # shared by all computers
+    my_rank = comm.Get_rank() # id for this computer
 
-    all_z_loss = []
+    p = comm.Get_size()       # number of processors
 
-    # loop over all redshifts
+    # MPI settings
+    # ----
+    n = len(all_combinations) # total number of loops used MPI
+    dest = 0                  # receiver destination processor
+    local_n = int(n / p)      # number of loops per processors;
+    assert (local_n - n/p) < 1e-5 # make sure n/p is integer
+
+    # Each MPI rank runs trough all zs
+    all_z_loss = np.full((len(data.zout), n), fill_value=np.nan, dtype=np.float)
+
+    all_z_local_loss = np.full((len(data.zout), local_n), fill_value=np.nan, dtype=np.float)
+
+    # loop over all redshifts, but separate the loop of combinations into MPI ranks
     for i, z in enumerate(data.zout):
 
         print("[Info] Getting flux vector at z = {:.3g} ...".format(z))
-
-        comm = MPI.COMM_WORLD     # shared by all computers
-        my_rank = comm.Get_rank() # id for this computer
-
-        p = comm.Get_size()       # number of processors
-
-        # MPI settings
-        # ----
-        n = len(all_combinations) # total number of loops used MPI
-        dest = 0                  # receiver destination processor
-        local_n = int(n / p)      # number of loops per processors;
-        assert (local_n - n/p) < 1e-5 # make sure n/p is integer
-
 
         # if not log scale, some selections cannot be trained, which might
         # indicate log scale is a better normalization for GP
@@ -57,8 +59,6 @@ def direct_search(
 
         # Start MPI loops
         # ----
-        total_loss = np.full((n, ), fill_value=np.nan, dtype=np.float) # Array to store all local loss
-
         local_i = my_rank * local_n                 # start point to run on this processor
         local_f = local_i + local_n                 # end   point to run on this processor
 
@@ -67,32 +67,34 @@ def direct_search(
         )
         local_loss = np.array(local_loss)
 
-        # the receiver rank
-        if my_rank == 0:
-            total_loss[local_i:local_f] = local_loss
+        all_z_local_loss[i, :] = local_loss
 
-            # receive the results for each mpi rank
-            for source in range(1, p):
-                local_loss = comm.recv(source=source)
-                print("Passing", my_rank, "<-", source)
 
-                local_i = source * local_n
-                local_f = local_i + local_n
+    # the receiver rank
+    if my_rank == 0:
+        all_z_loss[i, local_i:local_f] = all_z_local_loss
 
-                total_loss[local_i:local_f] = local_loss
-        # send message to the receiver rank
-        else:
-            comm.Send(local_loss, dest=dest)
-        
-        MPI.Finalize
+        # receive the results for each mpi rank
+        for source in range(1, p):
+            all_z_local_loss_i = np.full(all_z_local_loss.shape, fill_value=np.nan, dtype=np.float)
+            comm.Recv([all_z_local_loss_i, MPI.FLOAT], source=source)
+            print("Passing", my_rank, "<-", source)
 
-        all_z_loss.append(total_loss)
+            local_i = source * local_n
+            local_f = local_i + local_n
+
+            all_z_loss[i, local_i:local_f] = all_z_local_loss_i
+
+    # send message to the receiver rank
+    else:
+        comm.Send([all_z_local_loss, MPI.FLOAT], dest=dest)
+
+    MPI.Finalize
 
     # [sum over all redshifts] To account for all available redshift,
     # we sum the MSE loss from each z. The optimal one should have the
     # lowest average MSE loss across all z. 
-    loss_sum_z = np.array(all_z_loss).sum(axis=0)
-
+    loss_sum_z = np.nansum(all_z_loss, axis=0)
 
     # find the best samples to minimize the loss
     all_z_selected_index = [] # this is for individual z

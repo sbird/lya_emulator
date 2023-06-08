11ec0757264e30fefe53547c42a2791eec539555
Simeon Bird <sbird@ucr.edu>
2023-06-07 20:14:20 -0700

    Add chains to check against simulated data

--- plots/run_fernandez_2023_chains.py
+++ plots/run_fernandez_2023_chains.py
@@ -11,12 +11,8 @@ import sys
 # sys.path.append('path/to/emulator/code/')
 from lya_emulator_full.lyaemu import likelihood as lk
 from lya_emulator_full.lyaemu.meanT import t0_likelihood as t0lk
-basedir = 'current_emulator'
+basedir = '../dtau-48-48'
 chaindir = 'chains'
-
-
-
-
 
 ####################
 ######### meant only
@@ -58,7 +54,7 @@ like.do_sampling(savefile=chaindir+'fps-only/mf-48-z2.6-4.6-emuerr', burnin=1e4,
 # like.do_sampling(savefile=chaindir+'fps-only/mf-48-2xboss', burnin=1e4, nsamples=3e5, pscale=80, include_emu_error=True, use_meant=False, hprior='none', oprior=False, bhprior=False)
 
 
-# DR9 checks --- REQUIRES MANUALLY CHANGING THE DATA SET TO DR9 IN LIKELIHOOD.PY CALL TO lyman_data.BOSSData()
+# DR9 checks
 like = lk.LikelihoodClass(basedir, tau_thresh=1e6, optimise_GP=False, traindir=basedir+'/trained_mf', HRbasedir=basedir+'/hires', loo_errors=False, min_z=2.6, max_z=4.4, sdss="dr9")
 like.do_sampling(savefile=chaindir+'fps-only/mf-48-dr9-z2.6-4.4', burnin=1e4, nsamples=3e5, pscale=100, include_emu_error=True, use_meant=False, hprior='none', oprior=False, bhprior=False)
 
@@ -97,3 +93,14 @@ like.do_sampling(savefile=chaindir+'fps-meant/mf-48-48-z2.6-4.6-priors', burnin=
 
 like = lk.LikelihoodClass(basedir, tau_thresh=1e6, optimise_GP=False, traindir=basedir+'/trained_mf', HRbasedir=basedir+'/hires', loo_errors=False, min_z=2.2, max_z=4.6, use_meant=True)
 like.do_sampling(savefile=chaindir+'fps-meant/mf-48-48-z2.2-4.6-priors', burnin=1e4, nsamples=3e5, pscale=100, include_emu_error=True, use_meant=True, hprior='none', oprior=True, bhprior=True, meant_fac=mtf_fullz_emu)
+
+
+##### Likelihood function checks
+# Using a simulated HR dataset
+like = lk.LikelihoodClass(basedir, tau_thresh=1e6, optimise_GP=False, traindir=basedir+'/trained_mf', HRbasedir=basedir+'/hires', loo_errors=False, min_z=2.2, max_z=4.6, use_meant=True)
+#ns909 tau = 1.15
+like.do_sampling(datadir=basedir+'/hires', data_index=21, savefile=chaindir+'like-test/mf-48-48-z2.2-4.6', burnin=1e4, nsamples=3e5, pscale=100, include_emu_error=True, use_meant=True, hprior='none', oprior=False, bhprior=False, meant_fac=mtf_fullz_loo)
+#Using a simulated LR dataset with a different seed
+like = lk.LikelihoodClass(basedir, tau_thresh=1e6, optimise_GP=False, traindir=basedir+'/trained_mf', HRbasedir=None, loo_errors=False, min_z=2.2, max_z=4.6, use_meant=True)
+#ns909 tau = 1.15
+like.do_sampling(datadir=basedir+"/ns0.881-seed", data_index=0, savefile=chaindir+'like-test/seed', burnin=1e4, nsamples=3e5, pscale=100, include_emu_error=True, use_meant=False, hprior='none', oprior=False, bhprior=False)

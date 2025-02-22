254185b8f9d8c27a9aa5d61bf56d79b75150641e
Simeon Bird <sbird@ucr.edu>
2025-02-21 22:38:30 -0800

    Plot tweaks

--- plots/run_test_redlike.py
+++ plots/run_test_redlike.py
@@ -2,6 +2,7 @@
 from lyaemu import redlikelihood as rlk
 
 ##### Likelihood function checks
-rlk.do_sampling(like="ReducedLymanAlpha", savefile='chains/reduced-me')
-rlk.do_sampling(like="ChabReducedLymanAlpha", savefile='chains/reduced-chab')
-rlk.do_sampling(like="PlanckReduced", savefile='chains/reduced-planck')
+rlike = rlk.ReducedLymanAlpha()
+rlike.do_sampling(savefile='chains/reduced-me')
+rlike = rlk.ChabReducedLymanAlpha()
+rlike.do_sampling(savefile='chains/reduced-chab')
